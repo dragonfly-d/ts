c62683f6c2e065b7e340e57dc1b1fbebd62343bd
Dmitry <dragonfly_d@mail.ru>
2020-11-15 21:11:15 +0300

    prompt complete

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -22,7 +22,9 @@ function displayTodoList(): void {
 
 enum Commands {
     Add = "Add New Task",
+    Complete = "Complete Task",
     Toggle = "Show/Hide Completed",
+    Purge = "remove Completed Tasks",
     Quit = "Quit"
 }
 
@@ -38,6 +40,22 @@ function promptAdd(): void {
         }
         promptUser();
     });
+}
+
+function promptComplete(): void {
+    console.clear();
+    inquirer.prompt({
+        type: "checkbox", name: "complete",
+        message: "Mark Tasks Complete",
+        choices: collection.getTodoItems(showCompleted).map(item =>
+            ({ name: item.task, value: item.id, checked: item.complete }))
+    }).then(answers => {
+        let completedTasks = answers["complete"] as number[];
+        collection.getTodoItems(true).forEach(item =>
+            collection.markComplete(item.id,
+                completedTasks.find(id => id === item.id) != undefined));
+        promptUser();
+    })
 }
 
 function promptUser(): void {
@@ -58,6 +76,17 @@ function promptUser(): void {
             case Commands.Add:
                 promptAdd();
                 break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                } else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break
         }
     })
 }
