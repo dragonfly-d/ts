d64a7f06664c72fc0f636811d6ab8962201c7c6c
Dmitry <dragonfly_d@mail.ru>
2020-11-15 19:48:31 +0300

    inquirer.js added

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -1,3 +1,5 @@
+import * as inquirer from 'inquirer'
+
 import { TodoItem } from "./todoItem";
 import { TodoCollection } from "./todoCollection";
 
@@ -10,10 +12,34 @@ let todos: TodoItem[] = [
 
 let collection: TodoCollection = new TodoCollection("Dima", todos)
 
-console.clear();
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List `
+        + `(${collection.getItemCounts().incomplete} items to do)`);
 
-console.log(`${collection.userName}'s Todo List `
-    + `(${collection.getItemCounts().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
+
+enum Commands {
+    Quit = "Quit"
+}
+
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
+    inquirer.prompt({
+        type: "list",
+        name: "command",
+        message: "Choose option",
+        choices: Object.values(Commands)
+    }).then(answers => {
+        if (answers["command"] !== Commands.Quit) {
+            promptUser();
+        }
+    })
+}
+
+promptUser();
+
 
 // console.log(`${collection.userName}'s Todo List`);
 
@@ -21,5 +47,5 @@ console.log(`${collection.userName}'s Todo List `
 // let todoItem: TodoItem = collection.getTodoById(newId);
 // console.log(JSON.stringify(todoItem))
 // collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails());
+// collection.getTodoItems(true).forEach(item => item.printDetails());
 
